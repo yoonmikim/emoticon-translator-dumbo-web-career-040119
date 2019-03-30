f75e5c55da4640f953b445c9f2017a015f2eb6e4
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-30 02:21:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 def get_japanese_emoticon(file, emoticon)
   # code goes here
   library = load_library(file)
-  result = library
+  result = library["get_emoticon"]
 end
 
 def get_english_meaning
